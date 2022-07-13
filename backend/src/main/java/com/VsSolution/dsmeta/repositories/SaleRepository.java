01e373353f596df4e5f063abbad5ae2af19e9fcf
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-07-13 11:38:51 -0300

    API test

---
+++ backend/src/main/java/com/VsSolution/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.VsSolution.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.VsSolution.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/VsSolution/dsmeta/services/SaleService.java
@@ -0,0 +1,20 @@
+package com.VsSolution.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.VsSolution.dsmeta.entities.Sale;
+import com.VsSolution.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+}
